3ecb18bd0b1c5a1dea4b4e9c60d09a2726c8fa49
matthew <matthewbregg610@ufl.edu>
2016-01-27 14:15:47 -0500

    Added a collision sound, and a sound for the spidy
    Also tweaked
    driving values so car is drivable, flying still turns way to slow, I'll
    let robbie handle that.

---
+++ Assets/CollideSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollideSound : MonoBehaviour {
+    public AudioClip crashSound; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+   
+
+
+void OnCollisionEnter(Collision collision)
+    {
+        // next line requires an AudioSource component on this gameobject
+       GetComponent<AudioSource>().PlayOneShot(crashSound);
+    }
+}
