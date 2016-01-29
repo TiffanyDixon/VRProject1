ff2a84514758664ab2eef71590bded5266ab4c5d
matthew <matthewbregg610@ufl.edu>
2016-01-29 14:05:30 -0500

    Rather than giant spider, tree forces open gate!!

--- Assets/TreeFallWhenNearAndAddHaryPotterToCar.cs
+++ Assets/TreeFallWhenNearAndAddHaryPotterToCar.cs
@@ -2,6 +2,27 @@
 using System.Collections;
 
 public class TreeFallWhenNearAndAddHaryPotterToCar : MonoBehaviour {
+    private class RotateHelper {
+        private Transform from;
+        private Quaternion to;
+        private GameObject obj;
+        private float speed;
+        public RotateHelper(GameObject obj, Vector3 rotation, float speed) {
+            from = obj.transform;
+            from.Rotate(rotation);
+            to = new Quaternion(from.rotation.x, from.rotation.y, from.rotation.z, from.rotation.w);
+            from.Rotate(-rotation.x, -rotation.y, -rotation.z);
+            this.speed = speed;
+            this.obj = obj;
+        }
+     public void doRotateStep() {
+            obj.transform.rotation = Quaternion.Lerp(from.rotation, to, Time.deltaTime * speed);
+        }
+    public bool isDone() {
+            return from.rotation == to;
+        }
+
+    }
     public Transform target;
     // Use this for initialization
     public float distance;
@@ -10,36 +31,21 @@ public class TreeFallWhenNearAndAddHaryPotterToCar : MonoBehaviour {
     public GameObject gateLeft;
     public GameObject gateRight;
     public AudioClip TreeFallSound;
+    private RotateHelper treeRotate;
+    private System.Collections.Generic.List<RotateHelper> gateRotators;
 
-    void initializeToandFrom(ref Transform from,ref Quaternion to) {
-        from.Rotate(90, 0, 0);
-        to = new Quaternion(from.rotation.x, from.rotation.y, from.rotation.z, from.rotation.w);
-        from.Rotate(-90, 0, 0);
-    }
     void Start () {
         target = GameObject.FindWithTag("Player").transform; //target the player
-        fromTree = transform;
-        initializeToandFrom(ref fromTree,ref toTree);
-
-        fromGateL = gateLeft.transform;
-        fromGateR = gateRight.transform;
-        initializeToandFrom(ref fromGateL, ref toGateL);
-        initializeToandFrom(ref fromGateR, ref toGateR);
-
-
-
-
+        treeRotate = new RotateHelper(gameObject,new Vector3(90,0,0),speed);
+        gateRotators = new System.Collections.Generic.List<RotateHelper>();
+        gateRotators.Add(new RotateHelper(gateLeft, new Vector3(-90, 0, 0),speed/2));
+        gateRotators.Add(new RotateHelper(gateRight, new Vector3(-90, 0, 0), speed/2));
 
         //Rather than copy pasting the same line, should really put some of these in a utility class.
         //But that'd take time. 
 
     }
-    public Transform fromTree;
-    public Quaternion toTree;
-    public Transform fromGateL;
-    public Quaternion toGateL;
-    public Transform fromGateR;
-    public Quaternion toGateR;
+
     public float speed = 0.1F;
     public Camera playerCamera;
 
@@ -59,19 +65,19 @@ public class TreeFallWhenNearAndAddHaryPotterToCar : MonoBehaviour {
 
     }
 
-    void doRotateStep(Transform from, Quaternion to,float speed,Transform transformToRotate) {
-        transformToRotate.rotation = Quaternion.Lerp(from.rotation, to, Time.deltaTime * speed);
-    }
+
     void Update () {
         if (fallingDown && !done) {
-            doRotateStep(fromTree, toTree, speed,transform);
-            doRotateStep(fromGateL, toGateL, speed,gateLeft.transform);
-            doRotateStep(fromGateR, toGateR, speed,gateRight.transform);
+            treeRotate.doRotateStep();
+            foreach(RotateHelper rot in gateRotators) {
+                rot.doRotateStep();
+            }
+
             //Todo, refactor this into a class, then go through list of class and call rotate on each one, each class will hold the appropriate few vars.
 
         }
 
-        if (fromTree.rotation ==  toTree  && fallingDown) {
+        if (treeRotate.isDone()  && fallingDown) {
             done = true;
             carHarry.SetActive(true);
             treeHarry.SetActive(false);
