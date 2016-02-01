e28aef9b1c07cef822b8cff2cf9f08762678c6d2
bridgewaterrobbie <bridgewaterrobbie@gmail.com>
2016-02-01 00:01:28 -0500

    controls fixed
    fixed

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -4,12 +4,11 @@ using UnityStandardAssets.CrossPlatformInput;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
-    [RequireComponent(typeof(CarController))]
+    [RequireComponent(typeof (CarController))]
     public class CarUserControl : MonoBehaviour
     {
         private CarController m_Car; // the car controller we want to use
-        public float maxRollAngle = 80;
-        public float maxPitchAngle = 80;
+
 
         private void Awake()
         {
@@ -24,30 +23,21 @@ namespace UnityStandardAssets.Vehicles.Car
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
 
-            float roll = CrossPlatformInputManager.GetAxis("Horizontal");
-            float pitch = CrossPlatformInputManager.GetAxis("Vertical");
-            bool airBrakes = CrossPlatformInputManager.GetButton("Fire1");
-            //float throttle = airBrakes ? -1 : 1;
+            bool brakes = CrossPlatformInputManager.GetButton("Fire1");
 
+
+
+            
+
+
+            float r2 = CrossPlatformInputManager.GetAxis("R2");
+            float l2 = CrossPlatformInputManager.GetAxis("L2");
+
+            float throttle = r2 - l2;
+
+            
 
             float yaw = 0;
-            //float throttle = CrossPlatformInputManager.GetAxis("Triggers")*-1;
-             //For xbox its an axis
-
-            //float throttle = 0;
-			float r2 = CrossPlatformInputManager.GetAxis("R2");
-			float l2 = CrossPlatformInputManager.GetAxis("L2");
-			/*
-            if (l2)
-                throttle = -1;
-            if (r2)
-                throttle = 1;
-
-            if (r2 == l2)
-                throttle = 0;*/
-
-			float throttle = r2 - l2;
-                
 
             bool r1 = CrossPlatformInputManager.GetButton("R1");
             bool l1 = CrossPlatformInputManager.GetButton("L1");
@@ -59,50 +49,22 @@ namespace UnityStandardAssets.Vehicles.Car
             if (r1 == l1)
                 yaw = 0;
 
+   
+
 
             //#if !MOBILE_INPUT
-            float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+
+
+            float handbrake = CrossPlatformInputManager.GetAxis("Fire1");
             m_Car.Move(h, throttle, throttle, handbrake);
+          
+
+            m_Car.Move(h, v, yaw, throttle, brakes);
 
 
-            m_Car.Move(roll, pitch, yaw, throttle, airBrakes);
-            /*
-#else
-			AdjustInputForMobileControls(ref roll, ref pitch, ref throttle);
-
-            m_Car.Move(h, v, v, 0f);
-			m_Car.Move(roll, pitch, 0, throttle, airBrakes);*/
+            //#else
+            //          m_Car.Move(h, v, v, 0f);
             //#endif
-            if (Input.GetKeyDown("x"))
-            {
-                m_Car.switchMode();
-            }
-            if (CrossPlatformInputManager.GetButton("Fire3"))
-            {
-                m_Car.switchMode();
-            }
-
-
-        }
-
-        private void AdjustInputForMobileControls(ref float roll, ref float pitch, ref float throttle)
-        {
-            // because mobile tilt is used for roll and pitch, we help out by
-            // assuming that a centered level device means the user
-            // wants to fly straight and level!
-
-            // this means on mobile, the input represents the *desired* roll angle of the aeroplane,
-            // and the roll input is calculated to achieve that.
-            // whereas on non-mobile, the input directly controls the roll of the aeroplane.
-
-            float intendedRollAngle = roll * maxRollAngle * Mathf.Deg2Rad;
-            float intendedPitchAngle = pitch * maxPitchAngle * Mathf.Deg2Rad;
-            roll = Mathf.Clamp((intendedRollAngle - m_Car.RollAngle), -1, 1);
-            pitch = Mathf.Clamp((intendedPitchAngle - m_Car.PitchAngle), -1, 1);
-
-            // similarly, the throttle axis input is considered to be the desired absolute value, not a relative change to current throttle.
-            float intendedThrottle = throttle * 0.5f + 0.5f;
-            throttle = Mathf.Clamp(intendedThrottle - m_Car.Throttle, -1, 1);
         }
     }
 }
