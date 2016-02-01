0144c0b1a96df26fc7ff22e5ec07f4fdd424cc43
TiffanyDixon <TiffanyDixon94@gmail.com>
2016-01-31 19:49:11 -0500

    Added Train

---
+++ Assets/TrainMoving.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainMoving : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
