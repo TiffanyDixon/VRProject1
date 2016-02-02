a27938f24ba8203e99b5f2a2083a3b0aa9cd862c
matthew <matthewbregg610@ufl.edu>
2016-02-01 21:43:53 -0500

    The box dissapears after a bit to avoid slowdown

---
+++ Assets/DissapearAftercollide.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DissapearAftercollide : MonoBehaviour {
+    public float dissapearAfter;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            StartCoroutine(
+                DestroyAfterTime());
+            
+        }
+
+    }
+    IEnumerator DestroyAfterTime() {
+        yield return new WaitForSeconds(dissapearAfter);
+        Destroy(gameObject);
+    }
+}
