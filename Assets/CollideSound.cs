3518ec48f24d38f9741bdff79b96d17e3a497568
matthew <matthewbregg610@ufl.edu>
2016-02-03 02:33:34 -0500

    Made spiders chill the fuck out with a cooldown timer.
    So they won't make noise repeadetly anymore, and it won't blend together
    into a strange grating sound.

--- Assets/CollideSound.cs
+++ Assets/CollideSound.cs
@@ -5,6 +5,9 @@ public class CollideSound : MonoBehaviour {
     public AudioClip crashSound;
     public float distance = 200f;
     public Transform target;
+    public float coolDown = 0;
+
+    private float coolDownCounter = 0;
     // Use this for initialization
     void Start () {
         target = GameObject.FindWithTag("Player").transform; //target the player
@@ -12,17 +15,23 @@ public class CollideSound : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        coolDownCounter -= Time.deltaTime;
 	}
    
 
 
-void OnCollisionEnter(Collision collision)
-    {
+void OnCollisionEnter(Collision collision) { 
+
         if(Vector3.Distance(target.position, transform.position) > distance ) { 
             return; //Don't play sound if target is too far away.
         }
+        if (coolDownCounter >= 0) { //Next, check for the cooldown.
+            return;
+        }
         // next line requires an AudioSource component on this gameobject
         GetComponent<AudioSource>().PlayOneShot(crashSound);
+        //We played our sound, so reset the cooldown
+        coolDownCounter = coolDown;
     }
+
 }
