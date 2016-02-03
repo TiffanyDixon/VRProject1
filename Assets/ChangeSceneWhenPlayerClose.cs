3d60d740ec5e356aa119e05652066a3a4c0f233b
Matthew <matthewbregg610@ufl.edu>
2016-02-02 20:58:36 -0500

    Added invisible box to uending path

--- Assets/ChangeSceneWhenPlayerClose.cs
+++ Assets/ChangeSceneWhenPlayerClose.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class ChangeSceneWhenPlayerClose : MonoBehaviour {
     private Transform target;
     private Transform myTransform;
+    public float range;
 	// Use this for initialization
 	void Start () {
         target = GameObject.FindWithTag("Player").transform; //target the target
@@ -13,7 +14,7 @@ public class ChangeSceneWhenPlayerClose : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	    if ( Vector3.Distance(myTransform.position,target.position) < 50 ) {
+	    if ( Vector3.Distance(myTransform.position,target.position) < range ) {
             SceneManager.LoadScene("scene2.2");
         }
 	}
