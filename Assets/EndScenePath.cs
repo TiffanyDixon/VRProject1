9dc3d9bc2b512e26b06ccc812b538bf9281a6a23
matthew <matthewbregg610@ufl.edu>
2016-02-04 02:54:51 -0500

    Adjusted lighting and maybe someo ther stuff?

---
+++ Assets/EndScenePath.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScenePath : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("EndScenePath"), "time", 25, "easetype", "linear"));
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/TrainMoveLastScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainMoveLastScene : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("TrainMoveLastScene"), "time", 10, "easetype", "linear"));
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
